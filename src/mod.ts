d631b16628b8bd5696c5654182338e91246df4f3
Brennan Downs <brennan.downs111@gmail.com>
2025-03-26 15:20:36 -0700

    Updated to give the option to disable rain entirely. Also cleaned up code structure a bit

--- src/mod.ts
+++ src/mod.ts
@@ -1,55 +1,66 @@
-import { DependencyContainer } from "tsyringe";
-import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
-import { chosenSeason } from "../config/config.json";
-import { ConfigServer } from "@spt/servers/ConfigServer";
 import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
 import { IWeatherConfig } from "@spt/models/spt/config/IWeatherConfig";
+import { ConfigServer } from "@spt/servers/ConfigServer";
 import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
-import { setWeatherValues, SeasonMap, checkForWinter } from "./util";
+import { DependencyContainer } from "tsyringe";
+import { chosenSeason } from "../config/config.json";
+import { SeasonMap, checkForWinter, setWeatherValues } from "./util";
 
 class WeatherAndSeasonControl implements IPreSptLoadMod {
-    preSptLoad(container: DependencyContainer): void {
-        const serverConf = container.resolve<ConfigServer>("ConfigServer");
-        const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
-        this.setStaticSeason(serverConf);
-        this.registerWeatherUpdateRouter(staticRouterModService, container);
-    }
+	preSptLoad(container: DependencyContainer): void {
+		const serverConf = container.resolve<ConfigServer>("ConfigServer");
+		const staticRouterModService = container.resolve<StaticRouterModService>(
+			"StaticRouterModService",
+		);
+		this.setStaticSeason(serverConf);
+		this.registerWeatherUpdateRouter(staticRouterModService, container);
+	}
 
-    private setStaticSeason(serverConf: ConfigServer): void {
-        const WeatherConf = serverConf.getConfig<IWeatherConfig>(ConfigTypes.WEATHER);
-        const WeatherValues = WeatherConf.weather.seasonValues;
+	private setStaticSeason(serverConf: ConfigServer): void {
+		const weatherConf = serverConf.getConfig<IWeatherConfig>(
+			ConfigTypes.WEATHER,
+		);
+		const weatherValues = weatherConf.weather.seasonValues;
 
-        //set Season on startup
-        WeatherConf.overrideSeason = chosenSeason;
-        console.log("Your choice of season is:", SeasonMap[chosenSeason]);
+		//set Season on startup
+		weatherConf.overrideSeason = chosenSeason;
+		console.log(
+			"\x1b[35mYour choice of season is:",
+			SeasonMap[chosenSeason],
+			"\x1b[0m",
+		);
 
-        //set Weather on startup
-        setWeatherValues(WeatherValues[checkForWinter(chosenSeason)]);
-    }
+		//set Weather on startup
+		setWeatherValues(weatherValues[checkForWinter(chosenSeason)]);
+	}
 
-    private registerWeatherUpdateRouter(
-        staticRouterModService: StaticRouterModService,
-        container: DependencyContainer
-    ): void {
-        staticRouterModService.registerStaticRouter(
-            "WeatherAndSeasonControl",
-            [
-                {
-                    url: "/client/match/local/end",
-                    action: async (_url, _info, _sessionId, output) => {
-                        const configServer = container.resolve<ConfigServer>("ConfigServer");
-                        const WeatherConf = configServer.getConfig<IWeatherConfig>(ConfigTypes.WEATHER);
-                        const WeatherValues = WeatherConf.weather.seasonValues;
-                    
-                        //set Weather on raid end
-                        setWeatherValues(WeatherValues[checkForWinter(chosenSeason)]);
-                        return output;
-                    },
-                },
-            ],
-            "aki"
-        );
-    }
+	private registerWeatherUpdateRouter(
+		staticRouterModService: StaticRouterModService,
+		container: DependencyContainer,
+	): void {
+		staticRouterModService.registerStaticRouter(
+			"WeatherAndSeasonControl",
+			[
+				{
+					url: "/client/match/local/end",
+					action: async (_url, _info, _sessionId, output) => {
+						const configServer =
+							container.resolve<ConfigServer>("ConfigServer");
+						const weatherConf = configServer.getConfig<IWeatherConfig>(
+							ConfigTypes.WEATHER,
+						);
+						const weatherValues = weatherConf.weather.seasonValues;
+
+						//set Weather on raid end
+						setWeatherValues(weatherValues[checkForWinter(chosenSeason)]);
+						return output;
+					},
+				},
+			],
+			"aki",
+		);
+	}
 }
 
 module.exports = { mod: new WeatherAndSeasonControl() };
--- src/util.ts
+++ src/util.ts
@@ -1,29 +1,42 @@
-import { stormyWeatherPercentChance } from "../config/config.json";
 import { ISeasonalValues } from "@spt/models/spt/config/IWeatherConfig";
-import { defaultWeather, stormWeather } from "./weathervalues";
+import { stormyWeatherPercentChance } from "../config/config.json";
 import { chosenSeason } from "../config/config.json";
+import { defaultWeather, noRainWeather, stormWeather } from "./weathervalues";
 
-export const SeasonMap = ["SUMMER", "AUTUMN", "WINTER", "SPRING", "AUTUMN_LATE", "SPRING_EARLY", "STORM"];
+export const SeasonMap = [
+	"SUMMER",
+	"AUTUMN",
+	"WINTER",
+	"SPRING",
+	"AUTUMN_LATE",
+	"SPRING_EARLY",
+	"STORM",
+];
 
-export const checkForWinter = (chosenSeason: number) => {
-    if (chosenSeason === 2) {
-        console.log("Winter is coming!");
-        return "WINTER";
-    }
-    console.log("Winter is not coming!");
-    return "default";
-}
+export const checkForWinter = (chosenSeason: number): string => {
+	if (chosenSeason === 2) {
+		return "WINTER";
+	}
+	return "default";
+};
 
-export const setWeatherValues = (WeatherValues: ISeasonalValues) => {
-    let randomChance = Math.floor(Math.random() * 100 + 1);
-    if (stormyWeatherPercentChance > randomChance && chosenSeason !== 2) {
-        console.log("Weather is stormy!");
-        Object.assign(WeatherValues, stormWeather);
-    } else if (chosenSeason !== 2) {
-        console.log("Weather is normal!");
-        Object.assign(WeatherValues, defaultWeather);
-    } else {
-        console.log("Weather is winter!");
-        return; 
-    }
+export const setWeatherValues = (weatherValues: ISeasonalValues): void => {
+	const randomChance = Math.floor(Math.random() * 100 + 1);
+	switch (true) {
+		case stormyWeatherPercentChance === 0:
+			console.log("\x1b[35mWeather is never raining!\x1b[0m");
+			Object.assign(weatherValues, noRainWeather);
+			return;
+		case stormyWeatherPercentChance > randomChance && chosenSeason !== 2:
+			console.log("\x1b[35mWeather is stormy!\x1b[0m");
+			Object.assign(weatherValues, stormWeather);
+			return;
+		case chosenSeason !== 2:
+			console.log("\x1b[35mWeather is normal!\x1b[0m");
+			Object.assign(weatherValues, defaultWeather);
+			return;
+		default:
+			console.log("\x1b[35mWeather is winter!\x1b[0m");
+			return;
+	}
 };
