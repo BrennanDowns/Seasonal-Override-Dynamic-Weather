07c395b58f25c8bc644e47a545a6bc5d4b2c5d53
Brennan Downs <brennan.downs111@gmail.com>
2025-03-26 17:38:53 -0700

    Full rework, seasons now rotate every raid based on a weighting set in config.json. Weather unchanged.

--- src/mod.ts
+++ src/mod.ts
@@ -4,8 +4,14 @@ import { IWeatherConfig } from "@spt/models/spt/config/IWeatherConfig";
 import { ConfigServer } from "@spt/servers/ConfigServer";
 import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
 import { DependencyContainer } from "tsyringe";
-import { chosenSeason } from "../config/config.json";
-import { SeasonMap, checkForWinter, setWeatherValues } from "./util";
+import { seasonWeights } from "../config/config.json";
+import {
+	SeasonMap,
+	checkForWinter,
+	setWeatherValues,
+	weightedSeason,
+	remapSeasonWeights,
+} from "./util";
 
 class WeatherAndSeasonControl implements IPreSptLoadMod {
 	preSptLoad(container: DependencyContainer): void {
@@ -22,17 +28,15 @@ class WeatherAndSeasonControl implements IPreSptLoadMod {
 			ConfigTypes.WEATHER,
 		);
 		const weatherValues = weatherConf.weather.seasonValues;
+		const season = weightedSeason(remapSeasonWeights(seasonWeights));
+		const isWinter = season === 2;
 
 		//set Season on startup
-		weatherConf.overrideSeason = chosenSeason;
-		console.log(
-			"\x1b[35mYour choice of season is:",
-			SeasonMap[chosenSeason],
-			"\x1b[0m",
-		);
+		weatherConf.overrideSeason = season;
+		console.log("\x1b[35mSeason is:", SeasonMap[season], "\x1b[0m");
 
 		//set Weather on startup
-		setWeatherValues(weatherValues[checkForWinter(chosenSeason)]);
+		setWeatherValues(weatherValues[checkForWinter(season)], isWinter);
 	}
 
 	private registerWeatherUpdateRouter(
@@ -51,9 +55,13 @@ class WeatherAndSeasonControl implements IPreSptLoadMod {
 							ConfigTypes.WEATHER,
 						);
 						const weatherValues = weatherConf.weather.seasonValues;
+						const season = weightedSeason(remapSeasonWeights(seasonWeights));
+						const isWinter = season === 2;
 
 						//set Weather on raid end
-						setWeatherValues(weatherValues[checkForWinter(chosenSeason)]);
+						console.log("\x1b[35mSeason is now:", SeasonMap[season], "\x1b[0m");
+						weatherConf.overrideSeason = season;
+						setWeatherValues(weatherValues[checkForWinter(season)], isWinter);
 						return output;
 					},
 				},
--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,5 @@
 import { ISeasonalValues } from "@spt/models/spt/config/IWeatherConfig";
 import { stormyWeatherPercentChance } from "../config/config.json";
-import { chosenSeason } from "../config/config.json";
 import { defaultWeather, noRainWeather, stormWeather } from "./weathervalues";
 
 export const SeasonMap = [
@@ -13,25 +12,53 @@ export const SeasonMap = [
 	"STORM",
 ];
 
-export const checkForWinter = (chosenSeason: number): string => {
-	if (chosenSeason === 2) {
+export const remapSeasonWeights = (seasonWeights: {
+	[key: string]: number;
+}) => {
+	return Object.keys(seasonWeights).reduce(
+		(acc, key, index) => {
+			acc[index] = seasonWeights[key];
+			return acc;
+		},
+		{} as { [key: number]: number },
+	);
+};
+
+export const checkForWinter = (season: number): string => {
+	if (season === 2) {
 		return "WINTER";
 	}
 	return "default";
 };
 
-export const setWeatherValues = (weatherValues: ISeasonalValues): void => {
+export const weightedSeason = (SeasonWeight: { [key: number]: number }) => {
+	const totalWeight = Object.values(SeasonWeight).reduce((x, y) => x + y, 0);
+	const random = Math.floor(Math.random() * totalWeight);
+	let cumulative = 0;
+	for (let season = 0; season < Object.keys(SeasonWeight).length; season++) {
+		cumulative = cumulative + SeasonWeight[season];
+		if (random < cumulative) {
+			return season;
+		}
+	}
+	return 0; // Defaults to summer
+};
+
+export const setWeatherValues = (
+	weatherValues: ISeasonalValues,
+	isWinter: boolean,
+): void => {
 	const randomChance = Math.floor(Math.random() * 100 + 1);
 	switch (true) {
 		case stormyWeatherPercentChance === 0:
 			console.log("\x1b[35mWeather is never raining!\x1b[0m");
 			Object.assign(weatherValues, noRainWeather);
 			return;
-		case stormyWeatherPercentChance > randomChance && chosenSeason !== 2:
+		case stormyWeatherPercentChance > randomChance && !isWinter:
 			console.log("\x1b[35mWeather is stormy!\x1b[0m");
 			Object.assign(weatherValues, stormWeather);
 			return;
-		case chosenSeason !== 2:
+		case !isWinter:
 			console.log("\x1b[35mWeather is normal!\x1b[0m");
 			Object.assign(weatherValues, defaultWeather);
 			return;
